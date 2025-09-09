819676789ffcbd629331dea57be37ff3237da67f
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-09 12:48:42 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,10 @@
+package day2coding;
+public class Day2coding {
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World");
+        System.out.print("Hello ");
+        System.out.printf("World");
+    }
+    
+}
