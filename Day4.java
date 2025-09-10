f9713dcb5fbe1c25f8a070ebb233ce56fe3f449f
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-11 01:24:17 +0800

    Day4.java
    1. (\n) Menambah serta berpindah kursor ke baris baru.
    2. (\t) Menambah spasi horizontal 
    3. (\") Menambah tanda kutip ganda
    4. (\\) Menambah tanda garis miring terbalik itu sendiri

---
+++ Day4.java
@@ -0,0 +1,17 @@
+package day4coding;
+public class Day4coding {
+  public static void main(string) args) {
+    //kode (\n) berpindah kursor ke awal baris baru
+  System.out.println("Nama Saya Taufik\nUmur Saya 18 Tahun");
+
+    //kode (\t) menambah spasi horizontal
+  System.out.println("Tinggi:\t175cm");
+
+    //kode (\") menambah tanda kutip ganda didalam string
+  System.out.println("Pesan saya \"semangat untuk 100 day-nya dan semoga dimudahkan\" amin");
+
+    //kode (\\) menambahkan simbol garis miring terbaik itu sendiri
+  System.out.println("Ini adalah garis miring terbalik:\\");  
+  }
+
+}
