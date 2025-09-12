8eadd1e67f3661f0927a589aac7fdfcfe83f4961
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-12 21:45:58 +0800

    Day5.java
    byte : tipe data ini biasanya dipakai untuk menyimpan data kecil dan untuk menghemat memori .
    
    short : tipe data ini biasanya dipakai jika byte tidak bisa menampung banyak .
    
    int : tipe data ini biasanya sering dipakai pada bilangan bulat .
    
    long : tipe data ini biasanya dipakai jika integer tidak bisa menampung lebih banyak angka/bilangan bulat .

---
+++ Day5.java
@@ -0,0 +1,24 @@
+package pkgday5coding;
+public class Main {
+
+        public static void main(String[] args) {
+          //byte:
+          byte umur = 18;
+           
+          //short:
+          short tinggi = 175;
+           
+          //int:
+          int berat = 60;
+           
+          //long:
+          long harga = 500;
+           
+            System.out.println("umur : "+ umur +"tahun. ");
+            System.out.println("tinggi : "+ tinggi+"cm. ");
+            System.out.println("berat : "+ berat +"kg. ");
+            System.out.println("harga : "+ harga +"ribu. ");
+            
+        }
+    
+}
