66b18a97f0d875450c8c0c9546b1a972ea1156a4
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-14 19:06:18 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,19 @@
+package day7coding;
+public class Day7coding {
+    public static void main(String[] args) {
+        String nama = "MUHAMMAD TAUFIK";
+        System.out.println("Nama : " + nama);
+        
+        int umur = 18;
+        System.out.println("Umur : " + umur);
+        
+        //char (karakter)
+        char jenisKelamin = 'L';
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        
+        //boolean (boolean)
+        boolean sehat = true;
+        System.out.println("sehat : " + sehat);
+    }
+    
+}
