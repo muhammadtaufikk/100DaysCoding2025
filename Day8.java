c3992fd51e2fea8706547df6d8e56651f5cb48b9
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-15 10:54:10 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,13 @@
+package day8coding;
+public class Day5coding {
+    public static void main(String[] args) {
+        
+        // Deklarasi dan inisialisasi
+        String ucapan = "Assalamualaikum Warahmatullahi Wabarakatuh";
+        String nama = "Perkenalkan Nama Saya Muhammad Taufik";
+        
+        // Menampilkan isi String
+        System.out.println(salam);
+        System.out.println(nama);
+    }
+}
