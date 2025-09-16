72f505cd53c3fba6425cccd473f82bba66308ed3
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-16 12:53:44 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,33 @@
+package day9coding;
+public class Day9coding {
+    public static void main(String[] args) {
+
+// DEKLARASI VARIABEL
+    
+        int usia;
+        double tinggiBadan;
+        float beratBadan;
+        
+        // INISIALISASI VARIABEL
+        usia = 17;
+        tinggiBadan = 174.6;
+        beratBadan = 55.6f;
+        
+        System.out.println("Tahun 2024 ");
+        System.out.println("Usia : " + usia + " tahun");
+        System.out.println("Tinggi Badan : " + tinggiBadan + "cm");
+        System.out.println("Berat Badan : " + beratBadan + "kg");
+        
+        // UPDATE VARIABEL
+        usia = 18;
+        tinggiBadan = 175.0;
+        beratBadan = 60.0f;
+        
+        System.out.println("\nTahun 2025 ");
+        System.out.println("Usia : " + usia + " tahun");
+        System.out.println("Tinggi Badan : " + tinggiBadan + "cm");
+        System.out.println("Berat Badan : " + beratBadan + "kg");
+
+    }
+    
+}
