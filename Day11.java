0a466125966b41f79072468ebd571b0ea1c46a7f
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-18 00:54:07 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,16 @@
+package day11coding;
+import java.util.Scanner;
+public class Day11coding {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String user;
+        String password;
+        
+        System.out.print("masukkan user = ");
+        user = scan.nextLine();
+        System.out.print("masukkan password = ");
+        password = scan.nextLine();
+        System.out.println("\nSelamat Anda Berhasil Day 11 Coding");
+          
+    }  
+}
