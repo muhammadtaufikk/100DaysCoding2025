fe315fb47f8008119da8d59a560bdb876783c2b2
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-25 11:58:25 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,21 @@
+package day18coding;
+
+public class Day18coding {
+
+    public static void main(String[] args) {
+
+            int nilaiInt = 999;
+            long nilaiLong = nilaiInt;
+            
+            System.out.println("nilai int : " + nilaiInt);
+            System.out.println("nilai long : " + nilaiLong);
+            
+            float nilaiFloat = 25.2007f;
+            double nilaiDouble = nilaiFloat;
+            
+            System.out.println("\nnilai float : " + nilaiFloat);
+            System.out.println("nilai double : " + nilaiDouble);
+            
+    }
+    
+}
