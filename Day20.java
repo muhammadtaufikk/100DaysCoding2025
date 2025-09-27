c155546930e86156ce1a4143a864a250e19d4325
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-09-27 08:50:08 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,23 @@
+package pkgday20coding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //menggunakan metode value of:
+        short nilai =1000; 
+        String teks = String.valueOf(nilai);
+        System.out.println(nilai);
+        
+        //menggunakan metode .toString:
+        int angkaInt = 12345;
+        String konversi = Integer.toString(angkaInt);
+        System.out.println(konversi);
+                
+        //menggunakan metode operator:
+        long angka = 999999;
+        String output = angka + "";
+        System.out.println(output);
+                
+        }
+    
+}
