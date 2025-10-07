5b5e2f637b1b02e0aa0ecf46c56f06f1c261f732
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-10-07 20:05:53 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,17 @@
+package day30coding;
+public class Day30coding {
+    public static void main(String[] args) {
+        
+        
+        int nilai1 = 50;
+        int nilai2 = 95;
+
+        //(>=)
+        System.out.println("Apakah nilai1 >= nilai2? " + (nilai1 >= nilai2));
+
+        //(<=)
+        System.out.println("Apakah nilai1 <= nilai2? " + (nilai1 <= nilai2));
+
+
+    }
+}
