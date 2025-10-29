36ae60f07b8e3eedc3a032983f85b4e244c40d14
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-10-29 21:21:54 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+package day52coding;
+public class Day52coding {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Angka ke-" + i);
+            i++;
+        }
+    }
+}
