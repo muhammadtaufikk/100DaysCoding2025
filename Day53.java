5acb2e86301edc1849d87f3b996adb2dbe227b86
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-10-30 21:58:58 +0800

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,13 @@
+package day53coding;
+public class Day53coding {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("Angka: " + i);
+        }
+        System.out.println("Perulangan berhenti di angka 5");
+    }
+}
