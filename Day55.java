4df7a4f32674e541c8770197b365758f08dbadf2
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-11-01 20:09:04 +0800

    Add Day55.java

---
+++ Day55.java
@@ -0,0 +1,17 @@
+package day55coding;
+import java.util.Scanner;
+public class Day55coding {
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+        
+        System.out.print("masukkan nilai = ");
+        int nilai = l.nextInt();
+        
+        if (nilai >70) {
+            System.out.println("Ujian Kroco Ji Pale");
+        }else{
+            System.out.println("Sa Jappo'ka Aih");
+        }
+        
+    }
+}
