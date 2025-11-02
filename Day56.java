e4ffaa7dc329e283123849866d7a563471d404d3
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-11-02 20:37:22 +0800

    Day56.java

---
+++ Day56.java
@@ -0,0 +1,11 @@
+package day56coding;
+public class Day56coding {
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.println("Angka: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
