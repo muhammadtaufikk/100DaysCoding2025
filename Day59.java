78717ee70e04b3e56d1c97cf0fd332d16a22cd47
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-11-05 20:57:48 +0800

    Day59.java
    This program takes an integer input N and prints all odd and even numbers from 1 to N.

---
+++ Day59.java
@@ -0,0 +1,26 @@
+package day59coding;
+import java.util.Scanner;
+public class Day59coding {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+
+        System.out.println("Angka ganjil dari 1 hingga " + N + ":");
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Angka genap dari 1 hingga " + N + ":");
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
