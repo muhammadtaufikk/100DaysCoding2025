0624239b570c38f61dcabffbf72a8ffaf6ca9416
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-11-12 20:18:51 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,14 @@
+package day66coding;
+import java.util.Scanner;
+
+public class Day66coding {
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+        System.out.print("Masukkan tinggi (n): ");
+        int n = l.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+}
