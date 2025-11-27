30a7a845ab55fabeb34d8539513f843f397c0f95
Muhammad Taufik <mhmmdtaufik197@gmail.com>
2025-11-27 21:41:06 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,14 @@
+package day81coding;
+public class Day81coding {
+    public static void main(String[] args) {
+
+        int[] angka = {5, 10, 15, 20, 25};
+
+        System.out.println("Akses elemen array dengan indeks:");
+        System.out.println("Indeks 0 = " + angka[0]);
+        System.out.println("Indeks 1 = " + angka[1]);
+        System.out.println("Indeks 2 = " + angka[2]);
+        System.out.println("Indeks 3 = " + angka[3]);
+        System.out.println("Indeks 4 = " + angka[4]);
+    }
+}
